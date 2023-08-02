f8b8d7b49296294526d7abf5a9a7b9d82c869259
Hiep Nguyen <hiepskyb@gmail.com>
2023-08-03 01:00:06 +0700

    complete responsive design

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function toggleModal() {
     document.querySelector(".ticket-modal").classList.toggle("open");
 }
+
+function hideNav(event) {
+    if ($(this).is("#header li:last-child > a:first-child")) {
+        // Prevent default action
+        event.preventDefault();
+        console.log("dsdsd");
+      } else {
+        document.querySelector("#header").classList.toggle("nav-hide");
+      }
+}
+
+$("#header li:last-child > a:first-child").click(function(event) {
+    event.preventDefault();
+});
+
+document.querySelector(".mobile-menu-btn").addEventListener("click", hideNav);
+
+
+
+$("#header li a[href*='#']").not("#header li:last-child > a:first-child").click(hideNav);
+
+
+
+
